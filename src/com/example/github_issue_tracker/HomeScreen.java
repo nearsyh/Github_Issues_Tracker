c38e3c54012977d4b173e8069fa54c3d7c320b68
nearsyh <nearsy.h@gmail.com>
2014-02-10 17:21:46 -0500

    move the check of the username and reponame to the main view s.t. only one connection is necessary

--- src/com/example/github_issue_tracker/HomeScreen.java
+++ src/com/example/github_issue_tracker/HomeScreen.java
@@ -1,33 +1,24 @@
 package com.example.github_issue_tracker;
 
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Intent;
-import android.util.Log;
-//import android.text.Editable;
-//import android.text.TextWatcher;
-//import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
-public class HomeScreen extends Activity implements OnTaskCompleted {
+public class HomeScreen extends Activity { //implements OnTaskCompleted {
 	private String username, reponame;
 	private EditText user_name, repo_name;
-	private boolean isValid = true;
+	//private boolean isValid = true;
 	//private boolean isChecked = false, pressed = false;
-	AlertDialog errorDialog;
+	//AlertDialog errorDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        errorDialog = new AlertDialog.Builder(this).create();
-		errorDialog.setMessage("Username or Reponame are not correct");
+        //errorDialog = new AlertDialog.Builder(this).create();
+		//errorDialog.setMessage("Username or Reponame are not correct");
         user_name = ((EditText) findViewById(R.id.user_name));
     	repo_name = ((EditText) findViewById(R.id.repo_name));
     	/*
@@ -76,26 +67,34 @@ public class HomeScreen extends Activity implements OnTaskCompleted {
     	*/
     	username = user_name.getText().toString();
     	reponame = repo_name.getText().toString();
+    	Intent intent = new Intent(this, MainView.class);
+    	intent.putExtra("user_name", username);
+    	intent.putExtra("repo_name", reponame);
+    	startActivity(intent);
+		/*
     	try {
 			new ApiCall(this, true).execute(new URL(MainView.formURL(username, reponame)));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
+		*/
     }
 
+	/*
 	@Override
 	public void onTaskCompleted(String result) {
 		//isChecked = true;
-		isValid = !result.equals(ApiCall.CONNECTION_ERROR);
-		Log.v("homescreen", "" + isValid + " " + result + " " + ApiCall.CONNECTION_ERROR);
+		//isValid = !result.equals(ApiCall.CONNECTION_ERROR);
+		//Log.v("homescreen", "" + isValid + " " + result + " " + ApiCall.CONNECTION_ERROR);
 		//if(!pressed) return;
-		if(!isValid) {
-			errorDialog.show();
-		} else {
+		//if(!isValid) {
+			//errorDialog.show();
+		//} else {
 			Intent intent = new Intent(this, MainView.class);
 		    intent.putExtra("user_name", username);
 		    intent.putExtra("repo_name", reponame);
 		    startActivity(intent);
-		}
+		//}
 	}
+	*/
 }
--- src/com/example/github_issue_tracker/MainView.java
+++ src/com/example/github_issue_tracker/MainView.java
@@ -8,6 +8,7 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Intent;
 //import android.util.Log;
+//import android.util.Log;
 //import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -16,14 +17,14 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.widget.TextView;
+//import android.widget.TextView;
 
 // Contain the view
 @SuppressLint("DefaultLocale")
 public class MainView extends ListActivity implements OnTaskCompleted, OnScrollListener {
 	private String user, repo;
 	private ListView issues_list_view;
-	private InfoItemList issueData, commentData;
+	private InfoItemList data;
 	private HashMap<String, InfoItemList> commentsCache;
 	private AlertDialog commentDialog, loadingDialog;
 	@Override
@@ -33,7 +34,7 @@ public class MainView extends ListActivity implements OnTaskCompleted, OnScrollL
 		Intent intent = getIntent();
 	    user = intent.getStringExtra("user_name").toLowerCase();
 	    repo = intent.getStringExtra("repo_name").toLowerCase();
-	    issueData = new InfoItemList(this, formURL(user, repo), Issue.class);
+	    data = new InfoItemList(this, formURL(user, repo), Issue.class);
 	    issues_list_view = (ListView) findViewById(android.R.id.list);
 	    loadingDialog = new AlertDialog.Builder(this).create();
 	    loadingDialog.setMessage("Loading");
@@ -58,44 +59,42 @@ public class MainView extends ListActivity implements OnTaskCompleted, OnScrollL
 	@Override
 	public void onTaskCompleted(String result) {
 		loadingDialog.dismiss();
+		if(data.isReadingError()) {
+			commentDialog = new AlertDialog.Builder(this).create();
+			commentDialog.setMessage("Error with connection or the url");
+			commentDialog.show();
+			return;
+		}
+		InfoListAdapter adapter = new InfoListAdapter(this, data);
 		if(result.equals(Issue.class.getSimpleName())) {
-			InfoListAdapter adapter = new InfoListAdapter(this, issueData);
 			issues_list_view.setAdapter(adapter);
 			issues_list_view.setOnScrollListener(this);
 		} else {
-			if(commentData.isReadingError()) {
-				TextView errorInfo = new TextView(this);
-				errorInfo.setText("Error with connection or url");
-				commentDialog.setView(errorInfo);
+			ListView comments_list_view = new ListView(this);
+			if(data.getSize() == 0) {
+				commentDialog.setMessage("No comments for this issue");
 			} else {
-				InfoListAdapter adapter = new InfoListAdapter(this, commentData);
-				ListView comments_list_view = new ListView(this);
-				if(commentData.getSize() == 0) {
-					commentDialog.setMessage("No comments for this issue");
-				} else {
-					comments_list_view.setAdapter(adapter);
-					comments_list_view.setOnScrollListener(this);
-					commentDialog.setView(comments_list_view);
-				}
+				comments_list_view.setAdapter(adapter);
+				comments_list_view.setOnScrollListener(this);
+				commentDialog.setView(comments_list_view);
 			}
 			commentDialog.show();
-			
 		}
 	}
 
 	public void showCommentDialog(int position) {
+	    loadingDialog.show();
 		AlertDialog.Builder commentDialogBuilder = new AlertDialog.Builder(this);
-	    commentDialogBuilder.setTitle("Comments of " + ((Issue)issueData.getIssuesList().get(position)).getTitle());
-	    String lastCommentsURL = ((Issue)issueData.getIssuesList().get(position)).getCommentUrl();
+		Issue selected = (Issue)issues_list_view.getAdapter().getItem(position);
+	    commentDialogBuilder.setTitle("Comments of " + selected.getTitle());
+	    commentDialog = commentDialogBuilder.create();
+	    String lastCommentsURL = selected.getCommentUrl();
 	    if(commentsCache.containsKey(lastCommentsURL)) {
-	    	commentData = commentsCache.get(lastCommentsURL);
-	    	commentDialog = commentDialogBuilder.create();
+	    	data = commentsCache.get(lastCommentsURL);
 	    	onTaskCompleted(Comment.class.getSimpleName());
 	    } else {
-	    	commentData = new InfoItemList(this, lastCommentsURL, Comment.class);
-	    	loadingDialog.show();
-	    	commentDialog = commentDialogBuilder.create();
-	    	commentsCache.put(lastCommentsURL, commentData);
+	    	data = new InfoItemList(this, lastCommentsURL, Comment.class);
+	    	commentsCache.put(lastCommentsURL, data);
 	    }
 	}
 	
@@ -110,7 +109,7 @@ public class MainView extends ListActivity implements OnTaskCompleted, OnScrollL
 	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 		int loadedItems = firstVisibleItem + visibleItemCount;
 		if(((InfoListAdapter)view.getAdapter()).isFinished()) return;
-		if(loadedItems == totalItemCount){
+		else if(loadedItems == totalItemCount){
 			new LoadList((InfoListAdapter)view.getAdapter()).execute(loadedItems);
 		}
 	}
