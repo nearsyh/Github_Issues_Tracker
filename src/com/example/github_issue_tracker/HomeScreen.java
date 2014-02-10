6db03e33c47b5eee083d5a250e43c888c0c099f3
nearsyh <nearsy.h@gmail.com>
2014-02-10 17:23:00 -0500

    remove unneeded code from homescreen.java

--- src/com/example/github_issue_tracker/HomeScreen.java
+++ src/com/example/github_issue_tracker/HomeScreen.java
@@ -7,47 +7,13 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
-public class HomeScreen extends Activity { //implements OnTaskCompleted {
+public class HomeScreen extends Activity {
 	private String username, reponame;
 	private EditText user_name, repo_name;
-	//private boolean isValid = true;
-	//private boolean isChecked = false, pressed = false;
-	//AlertDialog errorDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        //errorDialog = new AlertDialog.Builder(this).create();
-		//errorDialog.setMessage("Username or Reponame are not correct");
-        user_name = ((EditText) findViewById(R.id.user_name));
-    	repo_name = ((EditText) findViewById(R.id.repo_name));
-    	/*
-    	username = user_name.getText().toString();
-		reponame = repo_name.getText().toString();
-		
-    	TextWatcher checkValid = new TextWatcher() {
-			@Override
-			public void afterTextChanged(Editable s) {
-				username = user_name.getText().toString();
-				reponame = repo_name.getText().toString();
-				try {
-					new ApiCall(HomeScreen.this, true).execute(new URL(MainView.formURL(username, reponame)));
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				}
-			}
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-				pressed = false;
-			}
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				isChecked = false;
-			}
-    	};
-    	user_name.addTextChangedListener(checkValid);
-    	repo_name.addTextChangedListener(checkValid);
-    	*/
     }
 
 
@@ -58,43 +24,11 @@ public class HomeScreen extends Activity { //implements OnTaskCompleted {
     }
     
     public void search(View v) {
-    	/*
-    	pressed = true;
-    	if(isChecked) {
-    		onTaskCompleted(isValid ? "Yeah" : "Error");
-    		return;
-    	}
-    	*/
     	username = user_name.getText().toString();
     	reponame = repo_name.getText().toString();
     	Intent intent = new Intent(this, MainView.class);
     	intent.putExtra("user_name", username);
     	intent.putExtra("repo_name", reponame);
     	startActivity(intent);
-		/*
-    	try {
-			new ApiCall(this, true).execute(new URL(MainView.formURL(username, reponame)));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		*/
     }
-
-	/*
-	@Override
-	public void onTaskCompleted(String result) {
-		//isChecked = true;
-		//isValid = !result.equals(ApiCall.CONNECTION_ERROR);
-		//Log.v("homescreen", "" + isValid + " " + result + " " + ApiCall.CONNECTION_ERROR);
-		//if(!pressed) return;
-		//if(!isValid) {
-			//errorDialog.show();
-		//} else {
-			Intent intent = new Intent(this, MainView.class);
-		    intent.putExtra("user_name", username);
-		    intent.putExtra("repo_name", reponame);
-		    startActivity(intent);
-		//}
-	}
-	*/
 }
